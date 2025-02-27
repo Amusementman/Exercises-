09e7e0bc4e81d21a7fb4a6f4bc64e13593097ae6
Amusementman man <harleytoshikian@gmail.com>
2025-02-27 20:04:03 +0000

    ye

--- Exercise5.py
+++ Exercise5.py
@@ -2,8 +2,14 @@
 
 menu = { "Pizza": 1.99, "Soda":  0.69, "Double Chunk Chocolate Chip Cookie": 2.49}
 
-def 
+def items_prices(item, price):
+    menu.update(item, price)
+
+items_prices(item = "watermelon", price = 0.78)
+print(str(menu))
 
 #Create a function that has two parameters, item and price, and adds that item and price into the menu dictionary.
-# Call the function with an item and price of your choosing and then print out the dictionary to
-# test if your function worked.
+#Call the function with an item and price of your choosing and then print out the dictionary to
+#test if your function worked.
+
+#NOT DONE
